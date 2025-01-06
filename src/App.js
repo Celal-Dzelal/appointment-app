05ed5ed26ffec5f23a9b1e64b2b165a5d005448d
Celal <binaycelalselim@gmail.com>
2025-01-06 12:37:16 +0300

    Removed All Files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import "./App.css";
-import AppointmentList from "./components/AppointmentList/AppointmentList";
-import Doctors from "./components/Doctors/Doctors";
-import Header from "./components/Header/Header";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Doctors />
-      <AppointmentList />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
